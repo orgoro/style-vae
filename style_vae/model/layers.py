5e4e71e7d0f08e60934593ad359b1f3cd375ed1e
orgoro <or.gorodissky@d-id.com>
2019-01-07 01:28:45 +0200

    layers_test: added

--- style_vae/model/layers.py
+++ style_vae/model/layers.py
@@ -5,41 +5,74 @@ from __future__ import division
 # 3rd party:
 import fire
 import tensorflow as tf
-from tensorflow import keras
+from tensorflow.keras import layers
 
 
 # same category:
-class Layers(object):
-
+class VaeLayers(object):
     @staticmethod
     def normalize(x, epsilon=1e-8):
         """Pixelwise feature vector normalization"""
-        with tf.variable_scope('Pixel-Norm'):
-            return x * tf.rsqrt(tf.reduce_mean(tf.square(x), axis=1, keepdims=True) + epsilon, name='norm-factor')
+        with tf.variable_scope('normalize'):
+            # return x * tf.rsqrt(tf.reduce_mean(tf.square(x), axis=1, keepdims=True) + epsilon, name='norm-factor')
+            mean, var = tf.nn.moments(x, axes=3)
+            std = tf.sqrt(var)
+            x = (x - mean[:, :, :, None]) / (std[:, :, :, None] + epsilon)
+            return x
 
     @staticmethod
     def additive_noise(x, noise):
         """scaling noise bi-cubic to fit x and adding it"""
         with tf.variable_scope('Additive-Noise'):
-            n = tf.image.resize_bicubic(noise, size=x.shape[1:3], name='resize-noise')[None]
+            n = tf.image.resize_bicubic(noise, size=x.shape[1:3], name='resize-noise')
             return x + n
 
     @staticmethod
     def stylize(x, style):
-        """stylize the feature maps"""
+        """stylize the feature maps AdaIN in paper"""
         with tf.variable_scope('Stylize'):
             style_len = tf.cast(tf.divide(tf.shape(style)[1], 2), tf.int64, name='style-len')
-            scale = style[:, :style_len]
-            bias = style[:, style_len:]
+            scale = style[:, None, None, :style_len]
+            bias = style[:, None, None, style_len:]
         return scale * x + bias
 
     @staticmethod
-    def cell_up(x):
-        pass
+    def conv3(x, f_maps, activation):
+        x = layers.Conv2D(filters=f_maps, kernel_size=3, padding='same', activation=activation)(x)
+        return x
 
     @staticmethod
-    def cell_down(x):
-        pass
+    def conv3_stride2(x, f_maps, activation):
+        x = layers.Conv2D(filters=f_maps, kernel_size=3, padding='same', strides=2, activation=activation)(x)
+        return x
+
+    @staticmethod
+    def cell_up(x, f_maps, noise, style, activation='relu'):
+        with tf.variable_scope('cell-up'):
+            x = layers.UpSampling2D(2)(x)
+            x = VaeLayers.conv3(x, f_maps, activation)
+            x = VaeLayers.additive_noise(x, noise)
+            x = VaeLayers.normalize(x)
+            x = VaeLayers.stylize(x, style)
+            x = VaeLayers.conv3(x, f_maps, activation)
+        return x
+
+    @staticmethod
+    def first_cell_up(self, var, f_maps, noise, style, activation='relu'):
+        with tf.variable_scope('first-cell-up'):
+            x = VaeLayers.additive_noise(var, noise)
+            x = VaeLayers.normalize(x)
+            x = VaeLayers.stylize(x, style)
+            x = VaeLayers.conv3(x, f_maps, activation)
+            x = VaeLayers.additive_noise(var, noise)
+            x = VaeLayers.normalize(x)
+            x = VaeLayers.stylize(x, style)
+        return x
+
+    @staticmethod
+    def cell_down(x, f_maps, activation='relu'):
+        x = VaeLayers.conv3_stride2(x, f_maps, activation)
+        return x
 
 
 if __name__ == '__main__':
--- style_vae/model/layers_test.py
+++ style_vae/model/layers_test.py
@@ -1,9 +1,76 @@
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import division
+
+# 3rd party:
+import tensorflow as tf
+import numpy as np
 import unittest
+
+# tested file:
+from style_vae.model.layers import VaeLayers
 
 
 class LayersTester(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, True)
+    sess = None
+
+    @classmethod
+    def setUpClass(cls):
+        tf.reset_default_graph()
+        cls.sess = tf.Session()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.sess.close()
+
+    def test_when_cell_up_then_scale_x2(self):
+        # ARRANGE:
+        size = 8
+        batch = 10
+        x = tf.ones((batch, size, size, 3))
+        noise = tf.ones((batch, size * 2, size * 2, 1))
+        style = tf.ones((batch, 4))
+
+        # ACT:
+        f_maps = 2
+        scaled_up = VaeLayers.cell_up(x, f_maps, noise, style)
+        self.sess.run(tf.global_variables_initializer())
+        result = self.sess.run(scaled_up)
+
+        # ASSERT:
+        self.assertEqual(result.shape, (batch, size * 2, size * 2, f_maps), 'shape must be b x 2Size x 2Size')
+
+    def test_when_cell_down_then_scale_half(self):
+        # ARRANGE:
+        size = 8
+        batch = 10
+        x = tf.ones((batch, size, size, 3))
+
+        # ACT:
+        f_maps = 2
+        scaled_down = VaeLayers.cell_down(x, f_maps)
+        self.sess.run(tf.global_variables_initializer())
+        result = self.sess.run(scaled_down)
+
+        # ASSERT:
+        self.assertEqual(result.shape, (batch, size // 2, size // 2, f_maps), 'shape must be b x 1/2Size x 1/2Size')
+
+    def test_when_normalize_then_unit(self):
+        # ARRANGE:
+        size = 8
+        batch = 10
+        f_maps = 15
+        x = tf.random_normal((batch, size, size, f_maps), mean=10, stddev=5)
+
+        # ACT:
+        normed = VaeLayers.normalize(x)
+        self.sess.run(tf.global_variables_initializer())
+        result = self.sess.run(normed)
+
+        # ASSERT:
+        np.testing.assert_almost_equal(np.mean(result, axis=3), np.zeros((batch, size, size)), 3, 'zero mean')
+        np.testing.assert_almost_equal(np.var(result, axis=3), np.ones((batch, size, size)), 3, 'unit var')
+        self.assertEqual(result.shape, (batch, size, size, f_maps), 'shape must not change')
 
 
 if __name__ == '__main__':
