d0c86b71c3a76b3b34ba1a6bfa2e4f553fbb086a
orgoro <or.gorodissky@d-id.com>
2019-02-02 19:38:08 +0200

    dataset: mnist64

--- style_vae/data/dataset.py
+++ style_vae/data/dataset.py
@@ -6,7 +6,7 @@ from __future__ import division
 from tensorflow.keras import datasets
 from dataclasses import dataclass
 import numpy as np
-
+import cv2
 
 @dataclass
 class Dataset:
@@ -33,7 +33,24 @@ class Dataset:
                        np.float32(x_train[:num_val] / 255.),
                        np.float32(x_test / 255.))
 
+    @classmethod
+    def get_mnist64(cls):
+        mnist = datasets.mnist.load_data()
+        (x_train, _), (x_test, _) = mnist
+        train_size = x_train.shape[0]
+        train_64 = np.array([cv2.resize(x_train[i], (64, 64)) for i in range(train_size)])
+        test_size = x_test.shape[0]
+        test_64 = np.array([cv2.resize(x_test[i], (64, 64)) for i in range(test_size)])
+        num_val = x_train.shape[0] // 10
+        return Dataset('mnist64',
+                       np.float32(train_64[num_val:] / 255.),
+                       np.float32(train_64[:num_val] / 255.),
+                       np.float32(test_64 / 255.))
+
 
 if __name__ == '__main__':
-    cifar10 = Dataset.get_cifar10()
-    print(cifar10)
+    mnist64 = Dataset.get_mnist64()
+    print(mnist64)
+    from matplotlib import pyplot as plt
+
+    plt.figure(), plt.gray(), plt.imshow(np.uint8(mnist64.train[0,:,:] * 255)), plt.colormaps('gray'),plt.show()
